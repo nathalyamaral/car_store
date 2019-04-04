b7daa8784fbb15a75af35c6290ef26a3ccefbb92
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-04-04 13:01:17 -0400

    config.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,17 +12,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js')}}" defer></script>
-    <script src="{{ asset('js/jquery-1.11.0.min.js')}}" defer></script>
-    <script src="{{ asset('js/jquery.gmap3.min.js')}}" defer></script>
-    <script src="{{ asset('js/custom.js')}}" defer></script>
-    <script src="{{ asset('js/bootstrap.js')}}" defer></script>
-    <script src="{{ asset('js/bootstrap.min.js')}}" defer></script>
-    <script src="{{ asset('js/owl.carousel.min.js')}}" defer></script>
-    <script src="{{ asset('js/slick.js')}}" defer></script>
-    <script src="{{ asset('js/sliderpro.min.js')}}" defer></script>
-    <script src="{{ asset('js/wow.animation.js')}}" defer></script>
-
+    <script src="{{ asset('js/app.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -28,9 +20,8 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
 </head>
-
+    
 <body>
     @header
     @endheader
