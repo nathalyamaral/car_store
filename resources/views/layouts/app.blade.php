1ee81071d3c18dadaf46c131dc789d042fc30c20
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-04-04 14:46:07 -0400

    Retirada de fontes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,8 +21,6 @@
     <script src="js/wow.animation.js" defer></script>
     <script src="js/custom.js" defer></script>
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
