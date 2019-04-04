7786b00c9aca9efabc4db76fe6bb12b7ad173215
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-04-04 18:26:47 -0400

    Correção arquivo custom.

--- resources/views/components/index/slide.blade.php
+++ resources/views/components/index/slide.blade.php
@@ -1,142 +1,141 @@
-	<script src="js/custom.js"></script> 
-	
-	<div class="Modern-Slider">
-		<!-- Slide 1 -->
-		<div class="item">
-			<div class="img-fill">
-				<img src="images/main_slide_01.jpg" alt="">
-				<div class="info">
-					<div>
-						<h5>ACROPOS HAS THE BEST CHOICE</h5>
-						<h3>Looking For Perffect <em>Car</em>?</h3>
-						<h6 class="secondary-button">
-							<a href="#">Find Your Car <i class="fa fa-car"></i></a>
-						</h6>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- // Slide 1 -->
-		
-		<!-- Slide 2 -->
-		<div class="item">
-			<div class="img-fill">
-				<img src="images/main_slide_02.jpg" alt="">
-				<div class="info">
-					<div>
-						<h5>ACROPOS HAS THE BEST CHOICE</h5>
-						<h3>Looking For Perffect <em>Car</em>?</h3>
-						<h6 class="secondary-button">
-							<a href="#">Find Your Car <i class="fa fa-car"></i></a>
-						</h6>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- // Slide 2 -->
-		
-		<!-- Slide 3 -->
-		<div class="item">
-			<div class="img-fill">
-				<img src="images/main_slide_03.jpg" alt="">
-				<div class="info">
-					<div>
-						<h5>ACROPOS HAS THE BEST CHOICE</h5>
-						<h3>Looking For Perffect <em>Car</em>?</h3>
-						<h6 class="secondary-button">
-							<a href="#">Find Your Car <i class="fa fa-car"></i></a>
-						</h6>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- // Slide 3 -->
-	</div>
 
-	<section class="top-slider-features wow fadeIn" data-wow-duration="1.5s">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="slider-top-features">
-						<div id="owl-top-features" class="owl-carousel owl-theme">
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_1.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+<div class="Modern-Slider">
+	<!-- Slide 1 -->
+	<div class="item">
+		<div class="img-fill">
+			<img src="images/main_slide_01.jpg" alt="">
+			<div class="info">
+				<div>
+					<h5>ACROPOS HAS THE BEST CHOICE</h5>
+					<h3>Looking For Perffect <em>Car</em>?</h3>
+					<h6 class="secondary-button">
+						<a href="#">Find Your Car <i class="fa fa-car"></i></a>
+					</h6>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // Slide 1 -->
+	
+	<!-- Slide 2 -->
+	<div class="item">
+		<div class="img-fill">
+			<img src="images/main_slide_02.jpg" alt="">
+			<div class="info">
+				<div>
+					<h5>ACROPOS HAS THE BEST CHOICE</h5>
+					<h3>Looking For Perffect <em>Car</em>?</h3>
+					<h6 class="secondary-button">
+						<a href="#">Find Your Car <i class="fa fa-car"></i></a>
+					</h6>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // Slide 2 -->
+	
+	<!-- Slide 3 -->
+	<div class="item">
+		<div class="img-fill">
+			<img src="images/main_slide_03.jpg" alt="">
+			<div class="info">
+				<div>
+					<h5>ACROPOS HAS THE BEST CHOICE</h5>
+					<h3>Looking For Perffect <em>Car</em>?</h3>
+					<h6 class="secondary-button">
+						<a href="#">Find Your Car <i class="fa fa-car"></i></a>
+					</h6>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- // Slide 3 -->
+</div>
+
+<section class="top-slider-features wow fadeIn" data-wow-duration="1.5s">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="slider-top-features">
+					<div id="owl-top-features" class="owl-carousel owl-theme">
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_1.jpg" alt=""></a>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_2.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_3.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_2.jpg" alt=""></a>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_4.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_5.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_3.jpg" alt=""></a>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_6.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_1.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_4.jpg" alt=""></a>
 							</div>
-							<div class="item car-item">
-								<div class="thumb-content">
-									<a href="single_car.html"><img src="images/car_item_2.jpg" alt=""></a>
-								</div>
-								<div class="down-content">
-									<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
-									<span>$36.000</span>
-								</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
+							</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_5.jpg" alt=""></a>
+							</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
+							</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_6.jpg" alt=""></a>
+							</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
+							</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_1.jpg" alt=""></a>
+							</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
+							</div>
+						</div>
+						<div class="item car-item">
+							<div class="thumb-content">
+								<a href="single_car.html"><img src="images/car_item_2.jpg" alt=""></a>
+							</div>
+							<div class="down-content">
+								<a href="single_car.html"><h4>Perfect Sport Car</h4></a>
+								<span>$36.000</span>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
-	</section>
-	
-	<!--@footer
-	@endfooter-->
+	</div>
+</section>
+
+<!--@footer
+@endfooter-->
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,12 +11,52 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
     
+    <!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
     <script src="js/jquery-1.11.0.min.js"></script>
-    <script src="js/sliderpro.min.js"></script>
-    <script src="js/slick.js"></script>
+
+	<!-- Slider Pro Js -->
+	<script src="js/sliderpro.min.js"></script>
+
+	<!-- Slick Slider Js -->
+	<script src="js/slick.js"></script>
+
+	<!-- Owl Carousel Js -->
     <script src="js/owl.carousel.min.js"></script>
+
+	<!-- Boostrap Js -->
     <script src="js/bootstrap.min.js"></script>
+
+    <!-- Boostrap Js -->
     <script src="js/wow.animation.js"></script>
+
+	<!-- Custom Js -->
+    <script src="js/custom.js"></script>
+
+    <!-- Fonts -->
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+    
+<body>
+    @header
+    @endheader
+</body>
+</html>
+
       
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
