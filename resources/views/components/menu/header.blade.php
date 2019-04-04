4af35042b205b50fd1b92c4292b5e5a9d5fc7682
NathalyAmaral <nathalyamaral07@gmail.com>
2019-04-04 20:11:18 -0300

    header

--- resources/views/components/menu/header.blade.php
+++ resources/views/components/menu/header.blade.php
@@ -6,26 +6,28 @@
 			</div>
 			<div id='cssmenu'>
 				<ul>
-				   	<li class='active'><a href="{{ url('/') }}">Homepage</a></li>
-			   		@if (Auth::guest())
-        				<li><a href="{{ route('login') }}">Login</a></li>
-				   		@if (Route::has('register'))
-					 	<li><a href="{{ route('register') }}">Register</a></li>
-						@endif
-                    @else
-                    	<li><a href="{{ url('/carros') }}">Lista de Carros</a></li>
-			   			<li><a href="#">Cadastrar Carros</a></li>
-			   			<li><a href="#">Cadastrar Agencia</a></li>
-                    	<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    	<li><a href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">Sair</a></li>
-                    @csrf
-                	</form>
+					<li class='active'><a href="{{ url('/') }}">Homepage</a></li>
+					<li><a href="{{ url('/carros') }}">Lista de Carros</a></li>
+					
+					@if (Auth::guest())
+					<li><a href="{{ route('login') }}">Login</a></li>
+					@if (Route::has('register'))
+					<li><a href="{{ route('register') }}">Register</a></li>
+					@endif
+					@else
+					<li><a href="#">Cadastrar Carros</a></li>
+					<li><a href="#">Cadastrar Agencia</a></li>
+					@csrf
+					<li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sair</a></li>
+					
+				</form>
 
-                    @endguest
-				</ul>
-			</div>
+				@endguest
+			</ul>
+			<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+				@csrf
+			</form>
 		</div>
 	</div>
+</div>
 </header>
