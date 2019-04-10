481f84dc602b56c0e0d395b9090ea974e2d6e285
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-04-10 17:19:33 -0400

    Correção format cpf.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -70,6 +70,7 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $data['cpf'] = preg_replace("/[^0-9]/", "", $data['cpf']);
         $sucessoUser = User::create([
             'cpf' => $data['cpf'],
             'name' => $data['name'],
