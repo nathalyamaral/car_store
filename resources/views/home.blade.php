e5b69a243fe5d9c7fd6d2a616e932b4de2a82287
DOUGLASMD1 <michaeldouglass.ferreira@gmail.com>
2019-04-14 13:13:12 -0400

    Ajustes de login.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,5 @@
-@extends('layouts.app')
-
-@section('content')
+@appview
+@endappview
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -20,4 +19,6 @@
         </div>
     </div>
 </div>
-@endsection
+
+@footer
+@endfooter
